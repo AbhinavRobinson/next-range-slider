3c256077cf6fb1ec10f32d3c5fdad62428a72fd4
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-08-07 16:12:38 +0530

    🥗 update properties

--- src/components/ReactMultiRangeSlider.tsx
+++ src/components/ReactMultiRangeSlider.tsx
@@ -1,20 +1,47 @@
 import React, { CSSProperties } from 'react';
 import './styles.css';
 
+export interface IThumbProps {
+  color?: string;
+  focusColor?: string;
+  diameter?: string;
+  yOffset?: string;
+  radius?: string;
+  borderRadius?: string;
+}
+
+export interface ITrackProps {
+  color?: string;
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+}
+
+export interface IRangeProps {
+  color?: string;
+}
+
+export interface IReactMultiRangeSliderOptions {
+  theme?: 'default' | 'dark';
+  thumb?: IThumbProps;
+  track?: ITrackProps;
+  range?: IRangeProps;
+  leftInputProps?: IReactInputProps;
+  rightInputProps?: IReactInputProps;
+}
+
 export interface IReactInputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
 
 export interface IReactMultiRangeSliderProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  theme?: 'default' | 'dark';
   min: number;
   max: number;
   step?: number;
-  inputLProps?: IReactInputProps;
-  inputRProps?: IReactInputProps;
+  options?: IReactMultiRangeSliderOptions;
 }
 
 const updateInputValues = (e: React.FormEvent<HTMLInputElement>) => {
-  let _t: any = e.target; // don't know type of this, but it works. :P
-  _t.parentNode.style.setProperty(`--${_t.id}`, +_t.value);
+  let _t: HTMLInputElement = e.target as HTMLInputElement;
+  _t.parentElement?.style.setProperty(`--${_t.id}`, _t.value);
 };
 
 const minMaxTags = (min: number, max: number) => {
@@ -26,27 +53,52 @@ const minMaxTags = (min: number, max: number) => {
   };
 };
 
-export const ReactMultiRangeSlider: React.FC<IReactMultiRangeSliderProps> = (props) => {
-  let { style, theme, min, max, step, inputLProps, inputRProps, ...rest } = props;
+const presets = {
+  default: {
+    '--thc': '#333',
+    '--thfc': '#444',
+    '--tcol': '#ccc',
+    '--rcol': '#555',
+    '--thd': '2rem',
+    '--thyoff': '-25%',
+    '--thr': '1rem',
+    '--thbr': '1rem',
+    '--tw': '100%',
+    '--th': '1rem',
+    '--tbr': '1rem'
+  },
+  dark: {
+    '--thc': '#eee',
+    '--thfc': '#fff',
+    '--tcol': '#333',
+    '--rcol': '#888',
+    '--thd': '2rem',
+    '--thyoff': '-25%',
+    '--thr': '1rem',
+    '--thbr': '1rem',
+    '--tw': '100%',
+    '--th': '1rem',
+    '--tbr': '1rem'
+  }
+};
 
-  /** EXTRACT PROPS */
-  let _inputLProps: IReactInputProps = { ...inputLProps },
-    _inputRProps: IReactInputProps = { ...inputRProps };
+export const ReactMultiRangeSlider: React.FC<IReactMultiRangeSliderProps> = (props) => {
+  let { style, min, max, step, options, ...wrapperProps } = props;
+  let { theme = 'default', leftInputProps, rightInputProps } = { ...options };
 
   /** OVERRIDE PROPS */
+  let _leftInputProps: IReactInputProps = { ...leftInputProps },
+    _rightInputProps: IReactInputProps = { ...rightInputProps };
   let _style: CSSProperties = {
     ...style,
+    ...presets[theme],
     ...minMaxTags(min, max)
   } as CSSProperties;
 
-  /** THEME STYLES */
-  if (theme === 'dark') {
-  }
-
   return (
-    <div className="wrap" role="group" aria-labelledby="multi-lbl" style={_style} {...rest}>
-      <input id="a" type="range" onInput={updateInputValues} {...{ max, min, step }} {..._inputLProps} />
-      <input id="b" type="range" onInput={updateInputValues} {...{ max, min, step }} {..._inputRProps} />
+    <div className="wrap" role="group" aria-labelledby="multi-lbl" style={_style} {...wrapperProps}>
+      <input id="a" type="range" onInput={updateInputValues} {...{ max, min, step }} {..._leftInputProps} />
+      <input id="b" type="range" onInput={updateInputValues} {...{ max, min, step }} {..._rightInputProps} />
     </div>
   );
 };
